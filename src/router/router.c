328e935d4d385d57e784b34479c4430a83f8c36f
x0djers <x0djers@gmail.com>
2025-05-27 18:38:10 +0300

    Add API routing for BMI calculation

--- src/router/router.c
+++ src/router/router.c
@@ -5,6 +5,8 @@
 #include "../lib/logger/log.h"
 #include "../constants/constants.h"
 #include "../lib/mongoose/mongoose.h"
+#include "../lib/json/json.h"
+#include "../services/bmi/bmi.h"
 
 static void sendFile(struct mg_connection *conn,
                       struct mg_http_message *httpMessage,
@@ -28,6 +30,53 @@ void routeRequest(struct mg_connection *conn, const int event, void *eventData) 
 
         } else if (mg_match(uri, mg_str("/result"), NULL)) {
             sendFile(conn, httpMessage, PATH_RESULT_HTML, MIME_HTML);
+
+        } else if (mg_match(uri, mg_str("/api/bmi"), NULL) &&
+                   mg_match(httpMessage->method, mg_str("POST"), NULL)) {
+            char *body_str = strndup(httpMessage->body.buf, httpMessage->body.len);
+            if (body_str == NULL) {
+                mg_http_reply(conn,
+                              500,
+                              "",
+                              "Internal Server Error");
+                return;
+            }
+
+            cJSON *json = cJSON_Parse(body_str);
+            free(body_str);
+
+            if (json == NULL) {
+                mg_http_reply(conn,
+                    400,
+                      "Content-Type: application/json\r\n",
+                      "{\"error\":\"Invalid JSON\"}");
+                return;
+            }
+
+            const cJSON *weight_json = cJSON_GetObjectItem(json, "weight");
+            const cJSON *height_json = cJSON_GetObjectItem(json, "height");
+            const cJSON *units_json = cJSON_GetObjectItem(json, "units");
+
+            if (!cJSON_IsNumber(weight_json) || !cJSON_IsNumber(height_json) || !cJSON_IsString(units_json)) {
+                cJSON_Delete(json);
+                mg_http_reply(conn, 400, "Content-Type: application/json\r\n", "{\"error\":\"Invalid parameters\"}");
+                return;
+            }
+
+            const float weight = weight_json->valuedouble;
+            const float height = height_json->valuedouble;
+            const char *units = units_json->valuestring;
+
+            const float bmi = calculateBMI(height, weight, units);
+
+            char response[100];
+            snprintf(response, sizeof(response), "{\"bmi\": %.2f}", bmi);
+
+            cJSON_Delete(json);
+
+            mg_http_reply(conn, 200,
+                          "Content-Type: application/json\r\n",
+                          "%s", response);
 
         } else if (mg_match(uri, mg_str("/css/*"), NULL) ||
                    mg_match(uri, mg_str("/js/*"), NULL)) {
