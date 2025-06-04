ee2bb4a680b78c96e94e153a56a08cc945f253f4
x0djers <x0djers@gmail.com>
2025-06-04 12:54:33 +0300

    Tear off the server type to its header file

--- backend/server/server.h
+++ backend/server/server.h
@@ -1,8 +1,21 @@
 #ifndef SERVER_H
 #define SERVER_H
 
-#include <constants/types.h>
 #include <errors/errors.h>
+
+typedef void (*Router)(struct mg_connection *, int, void *);
+
+typedef struct {
+    const char* host;
+    int port;
+    char fullUrl[512];
+} ServerAddress;
+
+typedef struct {
+    struct mg_mgr* mgr;
+    ServerAddress addr;
+    Router router;
+} Server;
 
 ErrorCode createServer(Server* server, ServerAddress address);
 
